6cb062cd527ad8b3693f4b46f7f2b5893f4dad71
00417709 <Li_YaMing@want-want.com>
2020-08-17 09:03:10 +0800

    vue项目初始化和升级

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
         ['link', { rel: 'icon', href: '/icon.ico' }]
     ],
     themeConfig: {
-        search:true,//关闭搜索框
+        search: true,//关闭搜索框
         searchMaxSuggestions: 10,
         lastUpdated: '上次更新时间',
         // GitHub仓库
@@ -16,7 +16,7 @@ module.exports = {
         repoLabel: 'My GitHub',
         nav: [
             // { text: '项目作品', link: '/project/music.md' },
-            { text: 'js', link: '/js/作用域和闭包.md'},
+            { text: 'js', link: '/js/作用域和闭包.md' },
             { text: 'html', link: '/html/模板.md' },
             { text: "css", link: '/css/其他.md' },
             { text: "vue", link: '/css/其他.md' },
@@ -30,84 +30,89 @@ module.exports = {
         ],
         sidebar: {
             '/other/': [
-                ['git','git'],
-           ],
+                ['git', 'git'],
+            ],
+            '/vue/': [
+                ['init', '项目初始化'],
+                ['update', '老项目升级'],
+            ],
             '/algorithm/': [
-                 ['heap','堆'],
-                 ['binary-search','二分查找法'],
+                ['heap', '堆'],
+                ['binary-search', '二分查找法'],
             ],
             '/js/': [
-                ['变量类型的分类及检测','变量类型的分类及检测'],
-                ['数据类型的转换','数据类型的转换'],
-                ['作用域和闭包','作用域和闭包'],
-                ['this的使用场景','this的使用场景'],
-                ['构造函数、原型','构造函数、原型'],
-                ['防抖和节流','防抖和节流'],
-                ['IIFE','IIFE'],
-                ['cookie storage','cookie storage'],
-                ['Ajax','Ajax']
+                ['string', 'string'],
+                ['变量类型的分类及检测', '变量类型的分类及检测'],
+                ['数据类型的转换', '数据类型的转换'],
+                ['作用域和闭包', '作用域和闭包'],
+                ['this的使用场景', 'this的使用场景'],
+                ['构造函数、原型', '构造函数、原型'],
+                ['防抖和节流', '防抖和节流'],
+                ['IIFE', 'IIFE'],
+                ['cookie storage', 'cookie storage'],
+                ['Ajax', 'Ajax']
             ],
             '/html/': [
-                ['模板','html模板'],
-                ['标签','html标签'],
-                ['svg','svg']
+                ['模板', 'html模板'],
+                ['标签', 'html标签'],
+                ['svg', 'svg']
             ],
             '/css/': [
-                ['元素类型','元素类型'],
-                ['css sprite','css sprite'],
-                ['position','position'],
-                ['图标库','图标库'],
-                ['其他','其他'],
-                ['样式选择器及优先级','样式选择器及优先级'],
-                ['一些样式','一些样式'],
+                ['元素类型', '元素类型'],
+                ['css sprite', 'css sprite'],
+                ['position', 'position'],
+                ['图标库', '图标库'],
+                ['其他', '其他'],
+                ['样式选择器及优先级', '样式选择器及优先级'],
+                ['一些样式', '一些样式'],
             ],
             '/nodeJS/': [
-                ['gulp','本地构建-gulp']
+                ['gulp', '本地构建-gulp']
             ],
             '/interview/': [
-                ['html相关','html相关'],
-                ['css相关','css相关'],
-                ['js相关','js相关'],
-                ['vue','vue'],
-                ['react','react'],
-                ['本地构建','本地构建'],
-                ['其它','其它']
+                ['html相关', 'html相关'],
+                ['css相关', 'css相关'],
+                ['js相关', 'js相关'],
+                ['vue', 'vue'],
+                ['react', 'react'],
+                ['本地构建', '本地构建'],
+                ['其它', '其它']
             ],
             '/weixin_miniprogram/': [
-                ['指南','指南'],
-                ['wxs','wxs'],
-                ['template','template'],
-                ['component','component'],
-                ['获取用户信息','获取用户信息'],
-                ['页面间数据传递','页面间数据传递'],
-                ['云开发','云开发'],
-                ['交互相关','交互相关'],
-                ['样式相关','样式相关'],
-                ['其他','其他']
+                ['指南', '指南'],
+                ['wxs', 'wxs'],
+                ['template', 'template'],
+                ['component', 'component'],
+                ['获取用户信息', '获取用户信息'],
+                ['页面间数据传递', '页面间数据传递'],
+                ['云开发', '云开发'],
+                ['交互相关', '交互相关'],
+                ['样式相关', '样式相关'],
+                ['其他', '其他']
             ],
             '/project/': [
-                ['music','音乐WebApp'],
-                ['ele','饿了么商家页面'],
-                ['news','新闻头条平台'],
+                ['music', '音乐WebApp'],
+                ['ele', '饿了么商家页面'],
+                ['news', '新闻头条平台'],
                 {
-                    title:'微信小程序-豆瓣电影',
-                    children:[
-                        ['/project/weixin-douban/项目介绍','项目介绍'],
-                        ['/project/weixin-douban/欢迎页','欢迎页'],
-                        ['/project/weixin-douban/新闻列表页','新闻列表页'],
-                        ['/project/weixin-douban/新闻详情页','新闻详情页']
+                    title: '微信小程序-豆瓣电影',
+                    children: [
+                        ['/project/weixin-douban/项目介绍', '项目介绍'],
+                        ['/project/weixin-douban/欢迎页', '欢迎页'],
+                        ['/project/weixin-douban/新闻列表页', '新闻列表页'],
+                        ['/project/weixin-douban/新闻详情页', '新闻详情页']
                     ]
 
                 },
                 {
-                    title:'小程序-期刊',
-                    children:[
-                        ['/project/weixin-jiudao/项目介绍','项目介绍']
+                    title: '小程序-期刊',
+                    children: [
+                        ['/project/weixin-jiudao/项目介绍', '项目介绍']
                     ]
                 },
-                ['to-do','Todo待办事项'],
-                ['tinyHeart','爱心鱼小游戏'],
-                ['blog','博客搭建'],
+                ['to-do', 'Todo待办事项'],
+                ['tinyHeart', '爱心鱼小游戏'],
+                ['blog', '博客搭建'],
             ],
         }
     }
--- docs/other/git.md
+++ docs/other/git.md
@@ -1,36 +1,39 @@
 ## 常用命令
+
 1. git add . 提交到暂存区
 2. git commit -m'' 提交到本地仓库
-3. git push origin master 提交到远程仓库master分支
+3. git push origin master 提交到远程仓库 master 分支
 4. git branch 当前分支
 5. git checkout xxx 还原文件或者切换分支
 6. git status
 7. git diff 查看不同
-8. git pull origin master  已有仓库，下载下来后同步到自己的本地
-9. git checkout -b xxx 新建分支并切换到分支b
-10. git merge dev 在master分支下合并dev 然后再
-11. git log --pretty=oneline  查看提交记录
-12. git reflog查看git提交历史 比如撤销过的版本号
+8. git pull origin master 已有仓库，下载下来后同步到自己的本地
+9. git checkout -b xxx 新建分支并切换到分支 b
+10. git merge dev 在 master 分支下合并 dev 然后再
+11. git log --pretty=oneline 查看提交记录
+12. git reflog 查看 git 提交历史 比如撤销过的版本号
 13. pwd //显示当前目录
 14. ls -ah //显示当前目录下的明细
 15. cat readme.txt//查看修改内容
 
-[常用命令by阮一峰](http://www.ruanyifeng.com/blog/2015/12/git-cheat-sheet.html)
+[常用命令 by 阮一峰](http://www.ruanyifeng.com/blog/2015/12/git-cheat-sheet.html)
 
 ## 在本地创建代码仓库并关联到远程
-笔记、课程地址(https://coding.imooc.com/learn/list/330.html)
-1. github上创建远程仓库
 
- GitHub 上创建一个 repo，填好 repo 名称，以及写一下 README，对项目先做个简单的描述。
+笔记、课程地址(https://coding.imooc.com/learn/list/330.html)
+
+1. github 上创建远程仓库
+
+GitHub 上创建一个 repo，填好 repo 名称，以及写一下 README，对项目先做个简单的描述。
 
 2. 创建本地代码仓库
 
-在命令行中，输入“git init”，使文件夹加入git管理
+在命令行中，输入“git init”，使文件夹加入 git 管理
 
 3. 关联远程分支
-首先在命令行中运行命令查看远程分支：```git remote -v```
+   首先在命令行中运行命令查看远程分支：`git remote -v`
 
-这里我们不会得到任何输出，因为我们还没有关联远程分支，我们先去 GitHub 上找到我们仓库的地址，在命令行运行：```git remote add origin 仓库地址```
+这里我们不会得到任何输出，因为我们还没有关联远程分支，我们先去 GitHub 上找到我们仓库的地址，在命令行运行：`git remote add origin 仓库地址`
 
 关联后，远程库的名字就是 `origin`，这是 `Git` 默认的叫法，也可以改成别的，但是 `origin` 这个名字一看就知道是远程库。
 
@@ -38,7 +41,7 @@
 
 4. 拉取代码
 
-运行如下命令从远程仓库拉取 master 分支代码并合并：```git pull origin master```
+运行如下命令从远程仓库拉取 master 分支代码并合并：`git pull origin master`
 
 这个时候会报错：
 
@@ -49,11 +52,10 @@ Please move or remove them before you merge.
 Aborting
 ```
 
-因为我们在初始化代码的时候也创建了 `README.md`，和远程仓库的 `README.md` 冲突了。我们把 `README.md` 文件删除，再次运行：```git pull origin master```
+因为我们在初始化代码的时候也创建了 `README.md`，和远程仓库的 `README.md` 冲突了。我们把 `README.md` 文件删除，再次运行：`git pull origin master`
 
-这次代码就拉取成功了，并且在本地也创建了一个 `master` 分支。 
+这次代码就拉取成功了，并且在本地也创建了一个 `master` 分支。
 
- 
 5. 提交代码
 
 最后我们来提交代码，首先运行：
@@ -67,69 +69,79 @@ git push origin master
 接着我们去 GitHub 仓库中就可以看到刚才这条提交记录了。
 
 ## 多分支
-master上为可以发布的代码，需要在分支上写自己负责的部分，ok后合并到master上
 
-- git branch 查看当前分支	
-- git checkout -b dev 创建新的分支dev并切换到dev
-- 提交到dev分支
-	- git add .
-	- git commit -m"备注"
-	- git push origin dev(分支名)
-- 合并到master上
+master 上为可以发布的代码，需要在分支上写自己负责的部分，ok 后合并到 master 上
 
-	（非提交的人合并时：
+- git branch 查看当前分支
+- git checkout -b dev 创建新的分支 dev 并切换到 dev
+- 提交到 dev 分支 - git add . - git commit -m"备注" - git push origin dev(分支名)
+- 合并到 master 上
 
-	- git fetch 把所有分支拉下来
-	- git checkout dev
-	- git pull origin dev
-	)
-	- git checkout master(切换到master分支)
-	- git pull origin master
-	- git merge dev
-	- git push origin master
+      	（非提交的人合并时：
+
+      	- git fetch 把所有分支拉下来
+      	- git checkout dev
+      	- git pull origin dev
+      	)
+      	- git checkout master(切换到master分支)
+      	- git pull origin master
+      	- git merge dev
+      	- git push origin master
+
+* 拉其他分支的远程代码
+* git checkout -b branch
+* git checkout —-track orgin/develop
 
 ## gitignore
-- **/foo: 忽略/foo, a/foo, a/b/foo等
+
+- \*\*/foo: 忽略/foo, a/foo, a/b/foo 等
 
 ## 撤销修改
+
 - git checkout --file//工作区时
-- git reset HEAD <file>//暂存区时，已经add了，先撤销提交，重新放回工作区
+- git reset HEAD <file>//暂存区时，已经 add 了，先撤销提交，重新放回工作区
 
 ## 版本回退
-用HEAD表示当前版本，上一个版本就是HEAD^，上上一个版本就是HEAD^^，当然往上100个版本写成HEAD~100	
+
+用 HEAD 表示当前版本，上一个版本就是 HEAD^，上上一个版本就是 HEAD^^，当然往上 100 个版本写成 HEAD~100
 
 ### 本地远程都回退
-- git reset --hard 1094a（版本号的前5位）
 
-此时git status 提示本地落后远程一个版本
+- git reset --hard 1094a（版本号的前 5 位）
+
+此时 git status 提示本地落后远程一个版本
 
 - git push origin 分支名 --force
 
-### 远程回退，保留本地的修改，即撤销push
-- git reset --soft 1094a（版本号的前5位）
+### 远程回退，保留本地的修改，即撤销 push
+
+- git reset --soft 1094a（版本号的前 5 位）
 - git add,commit
 - git push origin 分支名 --force
 
 ## 删除本地和远程分支
-- 删除本地分支 git branch -D dev
-- 删除远程分支  git push origin :dev
 
+- 删除本地分支 git branch -D dev
+- 删除远程分支 git push origin :dev
 
 ## 删除远程仓库文件
+
 - git pull origin master    # 将远程仓库里面的项目拉下来
 - dir                       # 查看有哪些文件夹
-- git rm -r --cached .idea  # 删除.idea文件
+- git rm -r --cached .idea  # 删除.idea 文件
 - git commit -m '删除.idea'  # 提交,添加操作说明
-- git push                   # 提交修改记录
+- git push # 提交修改记录
 
 ## 合并其他分支上的代码
+
 - git checkout master
 - git pull origin master
 - git checkout dev
 - git merge master
 
-## 将修改内容放到别的分支上 
-master为当前分支做了修改 dev为应该被修改的分支
+## 将修改内容放到别的分支上
+
+master 为当前分支做了修改 dev 为应该被修改的分支
 
 - git stash 将修改内容先暂存一边，让当前分支干净
 - git checkout -b dev
@@ -137,12 +149,14 @@ master为当前分支做了修改 dev为应该被修改的分支
 - git add . ……
 
 ## 修改远程仓库名
-- 查看当前文件夹关联的远程仓库 git remote -v	 
-- 解除当前文件夹与远程仓库的关联  git remote rm origin
-- 修改Github仓库名称和本地文件夹名称
-- 关联新的远程仓库git remote add origin 仓库的地址
+
+- 查看当前文件夹关联的远程仓库 git remote -v
+- 解除当前文件夹与远程仓库的关联 git remote rm origin
+- 修改 Github 仓库名称和本地文件夹名称
+- 关联新的远程仓库 git remote add origin 仓库的地址
 
 ## 上传项目时的一些问题
+
 ```js
 git push origin master
 To github.com:YaseminLi/react.git
@@ -156,6 +170,6 @@ hint: See the 'Note about fast-forwards' in 'git push --help' for details.
 
 可以尝试强行上传：
 
-$ git push -u origin +master
+\$ git push -u origin +master
 
 [其它问题](https://blog.csdn.net/crazydony/article/details/51983343)
