6b6629eacaff35a95be9fba054d3170eb927810d
00417709 <Li_YaMing@want-want.com>
2021-01-06 08:47:48 +0800

    105

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
-const path = require('path')
-function resolve (dir) {
-  return path.join(__dirname, dir)
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
 }
 module.exports = {
   devServer: {
     port: 8080,
-    open: true
+    open: true,
   },
   lintOnSave: true,
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.module
       .rule('md')
       .test(/\.md$/)
@@ -17,11 +18,12 @@ module.exports = {
       .end()
       .use('markdown-loader')
       .loader('markdown-loader')
-      .end()
+      .end();
     config.resolve.alias
       .set('@components', resolve('src/components'))
       .set('@views', resolve('src/views'))
       .set('@assets', resolve('src/assets'))
+      .set('images', resolve('./src/assets/images'));// 使用img(src='~images/loading.gif', alt='loading')
     // 若需要配置多个别名，后续紧跟着设置set即可
     // config.resolve.alias.set('@scss', resolve('src/static/scss')).set('@',resolve('src'))
   },
@@ -29,8 +31,8 @@ module.exports = {
     sourceMap: true,
     loaderOptions: {
       sass: {
-        prependData: '@import "@/assets/css/common.scss";'
-      }
-    }
-  }
-}
+        prependData: '@import "@/assets/css/common.scss";',
+      },
+    },
+  },
+};
